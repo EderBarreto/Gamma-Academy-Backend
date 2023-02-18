881079d54afa113a6b33f95da75ca67a5fca61f7
EderBarreto <edersonguimarees@hotmail.com>
2023-02-18 09:03:25 -0300

    primeiro commit

---
+++ exemplo01.java
@@ -0,0 +1,12 @@
+public package Aula01.Exemplos;
+
+//E chegou a hora de começar a brincar de programar em Java 
+//e ainda fazer versionamento de código
+
+class exemplo01 {
+
+        public static void main(String[] args) {
+            System.out.println("olá mundo bom!");
+        }
+    
+}
