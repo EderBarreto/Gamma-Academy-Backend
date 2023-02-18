64c4af4f388c517c9e879ebce698d693b900e5ff
EderBarreto <edersonguimarees@hotmail.com>
2023-02-18 09:52:58 -0300

    exemplo02

---
+++ Exemplo02.java
@@ -0,0 +1,12 @@
+package Aula01.Exemplos;
+
+public class Exemplo02 {
+    public static void main(String[] args) {
+        //operações matematicas
+        int a, b;
+        a = 2;
+        b = 3;
+        System.out.println("se a = 2 e b = 3 entao a + b = " + (a + b)) ;
+    }
+    
+}
